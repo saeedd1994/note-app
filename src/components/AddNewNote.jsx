9b9b6aa53eab99d984b40306b94a0fcc72c2d30d
Saeed Dashti <saeed.da1994@gmail.com>
2025-09-22 23:08:50 +0330

    save target value in state generate structure of note object

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,19 +1,37 @@
+import {useState} from "react";
+
 function AddNewNote() {
+
+    const [title, setTitle] = useState('')
+    const [desc, setDesc] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('event : ', e);
+        const newNote = {
+            title,
+            desc,
+            id: Date.now(),
+            completed: false,
+            createdAt: new Date().toISOString(),
+        }
+        console.log(newNote)
+        setTitle('');
+        setDesc('');
     }
 
     return (
         <div className="add-new-note">
             <h1>Add New Note</h1>
             <form className='note-form' onSubmit={handleSubmit}>
-                <input type="text" className='text-field' placeholder='Note Title'/>
-                <input type="text" className='text-field' placeholder='Note Description'/>
+                <input value={title} onChange={e => setTitle(e.target.value)} type="text" className='text-field'
+                       placeholder='Note Title'/>
+                <input value={desc} onChange={e => setDesc(e.target.value)} type="text" className='text-field'
+                       placeholder='Note Description'/>
                 <button className='btn btn--primary'>Add New Note</button>
 
             </form>
         </div>
     )
-}export default AddNewNote
+}
+
+export default AddNewNote
