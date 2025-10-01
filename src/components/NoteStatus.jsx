70304439c5e9afdac15c7899ba4815857d456e2c
Saeed Dashti <saeed.da1994@gmail.com>
2025-10-01 12:32:07 +0330

    dynamic component using children props

--- src/components/NoteStatus.jsx
+++ src/components/NoteStatus.jsx
@@ -1,15 +1,19 @@
+import Message from "./Message.jsx";
+
 function NoteStatus({notes}) {
     const allNotes = notes.length;
     const completedNotes = notes.filter((note) => note.completed).length;
     const openNotes = allNotes - completedNotes;
     if (!allNotes) {
-        return <h2>No Notes Has Already Been Added...</h2>
+        return (<Message>
+            <span>ℹ️</span>  No Notes Has Already Been Added...
+        </Message>)
     }
     return (
         <ul className="note-status">
-           <li>all <span>{allNotes}</span></li>
-           <li>completed <span>{completedNotes}</span></li>
-           <li>open <span>{openNotes}</span></li>
+            <li>all <span>{allNotes}</span></li>
+            <li>completed <span>{completedNotes}</span></li>
+            <li>open <span>{openNotes}</span></li>
         </ul>
     )
 }
